b0303d786ad31270749f5b039864300e903bf2ab
Matteo Papadopoulos <m.papadopoulos@cantierecreativo.net>
2018-03-03 01:35:21 +0100

    Addthis to share articles

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,9 +1,7 @@
 import modernizr from 'modernizr';
 
 var el = document.querySelector('.js-side-bar-toggler');
-console.log(el.parentElement);
 
 el.onclick = function() {
-  console.log("clic");
   el.parentElement.classList.toggle('is-active');
 }
